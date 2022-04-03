00aa6e498b6af5728258bd9a513eaa53b8f5cf1d
momo <samomomoekai@gmail.com>
2022-04-03 19:44:01 +0900

    first-commit

--- SampleTableView/ViewController.swift
+++ SampleTableView/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+//DataSource：セルの個数や中身
+//ex)表示させるセルの個数を設定するメソッド
 
-class ViewController: UIViewController {
+//Delegate：イベント発生時の処理を移譲
+//ex)タップされたときの処理を書くメソッドが用意
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let fruit = ["りんご","ぶどう","みかん","レモン","スイカ"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //tableViewの中のRow(＝行)の数を返す関数,
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruit.count
+    }
+    
+    //
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = fruit[indexPath.row]
+        return cell
+    }
 
 }
 
